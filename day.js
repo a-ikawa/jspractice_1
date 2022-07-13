d3a9878c3dfeb053c6dad0b35bb7d0f0414d488a
Ayaka Ikawa <727hwj@gmail.com>
2022-07-13 11:23:58 -0500

    updated the day.js

--- day.js
+++ day.js
@@ -2,24 +2,24 @@ let day = 'wednesday';
  
 switch (day) {
   case 'monday':
-    console.log('today is monday');
+    console.log('five days until saturday');
     break;
   case 'tuesday':
-    console.log('today is tuesday');
+    console.log('four days until saturday');
     break;
   case 'wednesday':
-    console.log('today is wednesday');
+    console.log('three days until saturday');
     break;
-    case 'thursday':
-        console.log('today is thursday');
-        break;
-case 'friday':
-            console.log('today is friday');
-            break;
-case 'saturday'
-console.log('today is saturday');
-            break;
+  case 'thursday':
+    console.log('two days until saturday');
+    break;
+  case 'friday':
+    console.log('one days until saturday');
+    break;
+  case 'saturday': 
+    console.log('today is saturday');
+    break;
   default:
-    console.log('Invalid item');
+    console.log(`The correct day is ${day}`);
     break;
 }
